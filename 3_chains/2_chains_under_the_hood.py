700d61fe288553a9a8ee297fe123c068b0d71dc5
Nguyễn Chí Cường <cuongnguyengl2003@gmail.com>
2024-09-23 21:58:55 +0700

    chains with runnable

---
+++ 3_chains/2_chains_under_the_hood.py
@@ -0,0 +1,32 @@
+from dotenv import load_dotenv
+from langchain.prompts import ChatPromptTemplate
+from langchain.schema.runnable import RunnableLambda, RunnableSequence
+from langchain_openai import ChatOpenAI
+
+# Load  environment variables from .env
+load_dotenv()
+
+# Create a ChatOpenAI model
+model = ChatOpenAI(model="gpt-4o-mini")
+
+# Define the prompt template
+prompt_template = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are a comedian who tells jokes about {topic}."),
+        ("human", "Tell me {joke_count} jokes."),
+    ]
+)
+
+
+# Create individual runnables
+format_prompt = RunnableLambda(lambda x: prompt_template.format_prompt(**x))
+invoke_model = RunnableLambda(lambda x: model.invoke(x.to_messages()))
+parse_output = RunnableLambda(lambda x: x.content)
+
+# Create a sequence of runnables
+chain = RunnableSequence(first=format_prompt, middle=[invoke_model], last=parse_output)
+
+response = chain.invoke({"topic": "lawyers", "joke_count": 3})
+
+# Output
+print(response)
